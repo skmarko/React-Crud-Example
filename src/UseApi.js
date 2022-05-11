c926cc649ea2c35673b8438a72b1569ca1eca476
Code With Marko G <skamrk516@gmail.com>
2022-05-11 12:45:21 +0530

    intial comment

---
+++ src/UseApi.js
@@ -0,0 +1,19 @@
+import React from "react";
+import App from "./App";
+const UseApi = ()=>{
+    return (
+<>
+<h2>Github User List</h2>
+<div className="container-fluid mt-5">
+<div className="row text-center">
+    <div className=" col-10 col-md-4 mt-5">
+
+    </div>
+
+</div>
+</div>
+</>
+
+    )
+}
+export default UseApi
